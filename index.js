9e4bb474a430be4072d43e9939c1e29500f30f5c
sebacarrasco <sebacarrasco@uc.cl>
2023-03-19 18:12:19 -0300

    Each phrase in a separate paragraph

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const writeMd = (path, text) => fs.writeFile(path, text);
 // y texto con mayuscula despues de un espacio despues
 const addSpacesBeforePhrase = (text, n) => _.replace(text, /(?<=\S)\.\s(?=[A-Z])/g, _.padEnd('.', n));
 
+const eachPhraseInAParagraph = (text) => _.replace(text, /(?<=\S)\.\s(?=[A-Z])/g, '.\n\n');
+
 const main = async () => {
   let md = await readMd('test.md');
   md = addSpacesBeforePhrase(md, 5);
+  md = eachPhraseInAParagraph(md);
   await writeMd('test1.md', md);
 };
 
